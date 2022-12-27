40976a2265e1fa1f0904604bbaa8789a00b17b4b
WhiteFlaw <71926259+WhiteFlaw@users.noreply.github.com>
2022-12-27 08:37:16 +0800

    获取的DOM元素是何种类型

---
+++ DOM类型.ts
@@ -0,0 +1,7 @@
+let body: HTMLElement = document.body;
+
+let allDiv: NodeList = document.querySelectorAll('div');
+
+document.addEventListener('click', (e: MouseEvent) => {
+    e.preventDefault();
+})
